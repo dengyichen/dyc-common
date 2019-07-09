ff9249da7e5f7c17fb055eddf91958f18864e1c2
dengyichen <dengyichen@geexfinance.com>
2019-07-09 13:21:43 +0800

    增加正浮点数验证

--- js/common/verification.js
+++ js/common/verification.js
@@ -1,9 +1,9 @@
 export default {
-  //判断正整数包含0
+  //验证正整数包含0
   positiveIntVer(num){
     return /^([1-9]\d*|[0]{1,1})$/.test(num);
   },
-  //判断最少2个汉字
+  //验证最少2个汉字
   Chinese2Ver(str){
     return /[\u4e00-\u9fa5]{2,}/.test(str);
   },
@@ -11,7 +11,13 @@ export default {
   mobileVer(mobile){
     return /^1[345789]\d{9}$/.test(mobile);
   },
-  bankCardVer(){
-    return /^([1-9]{1})(\d{14}|\d{18})$/.test(mobile);
+  //验证银行卡号
+  bankCardVer(bnkCard){
+    return /^([1-9]{1})(\d{14}|\d{18})$/.test(bnkCard);
+  },
+  //验证正浮点数
+  keepDecimalVer(num,digit = 2){
+    let reg = new RegExp(`^\\d+(\\.\\d{1,${digit}})?$`);
+    return reg.test(num);
   }
 }
