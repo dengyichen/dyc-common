6052d8fb9dae05c3e6238358798cd5e1d2b7a95e
dengyichen <dengyichen@geexfinance.com>
2020-03-30 15:34:44 +0800

    增加自定义指令：拖拽

---
+++ js/common/directive.js
@@ -0,0 +1,22 @@
+export default {
+    //拖拽指令 value是距离DOM顶部的有效拖拽像素
+    drag(el, binding){
+        el.onmousedown = (e) => {
+            //如果鼠标点击的位置在标题块中则执行拖拽
+            if(e.clientY <= el.offsetTop + binding.value){
+                //算出鼠标相对元素的位置
+                let disX = e.clientX - el.offsetLeft;
+                let disY = e.clientY - el.offsetTop;
+                document.onmousemove = (e)=>{
+                    //移动当前元素
+                    el.style.left = `${e.clientX - disX}px`;
+                    el.style.top = `${e.clientY - disY}px`;
+                };
+                document.onmouseup = (e) => {
+                    document.onmousemove = null;
+                    document.onmouseup = null;
+                };
+            }
+        };
+    }
+}
--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@
 import common from './common/common.js'
 import verification from './common/verification.js'
 import filter from './filter/filter.js'
+import directive from './common/directive.js'
 
 module.exports = {
   common,
   verification,
-  filter
+  filter,
+  directive
 }
